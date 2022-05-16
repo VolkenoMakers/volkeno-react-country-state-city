5dd1be208b9b3e8a51e9203bfc0042b6fc9af152
Mbaye Niass <mbayeniass19@gmail.com>
2022-05-16 18:03:54 +0000

    Updates

---
+++ precache-manifest.ac10f732856907cc2ba75f50ae3e2f4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bf7d056151fc9b5d7c38900b396cc6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "908bb054bdf94e5eda22",
+    "url": "./static/css/main.990cc917.chunk.css"
+  },
+  {
+    "revision": "cfeb2a4c978bb8d1b57f",
+    "url": "./static/js/2.29cb1b0c.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "./static/js/2.29cb1b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908bb054bdf94e5eda22",
+    "url": "./static/js/main.d6d63c78.chunk.js"
+  },
+  {
+    "revision": "ca8d2b1353a88d6fe0d9",
+    "url": "./static/js/runtime-main.773dd46d.js"
+  }
+]);
